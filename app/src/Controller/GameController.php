fdc3790375f4829f7622c8a79d380e72228f2a08
denis-korotkov <deniskorotkov99@gmail.com>
2024-08-11 13:08:21 +0700

    Добавил header.twig

--- app/src/Controller/GameController.php
+++ app/src/Controller/GameController.php
@@ -3,11 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Game;
-use App\Entity\Result;
-use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,27 +21,5 @@ class GameController extends AbstractController
     public function show(Game $game): Response
     {
         return $this->render('gameAdd.twig', ['game'=> $game->getId(), 'fields' => $game->getFields()]);
-    }
-
-    #[Route('/{game}', methods: ['POST'])]
-    public function post(Request $request, LoggerInterface $logger, Game $game, EntityManagerInterface $entityManager): Response
-    {
-        $payload = $request->getPayload();
-
-        //todo сервис для валидации и простваления значений по результатам
-
-        $result = new Result();
-        $result->setGame($game);
-        $result->setValue([
-            'Result' => $payload->get('Result'),
-            'Date' => $payload->get('Date'),
-            'Players' => $payload->get('Players'),
-            'Duration' => $payload->get('Duration'),
-        ]);
-
-        $entityManager->persist($result);
-        $entityManager->flush();
-
-        return new RedirectResponse('/');
     }
 }
