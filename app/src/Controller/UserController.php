ddcb990e64585c15b261706e27e66d14480d643d
denis-korotkov <deniskorotkov99@gmail.com>
2025-04-04 13:53:25 +0700

    Начал формировать список последних игр. Рефактор шаблонов

---
+++ app/src/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Game;
+use App\Service\FieldService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/my-games', methods: ['GET'])]
+    public function get(Request $request, Game $game, FieldService $fieldService): Response
+    {
+        return $this->render('game.twig', [
+            'game' => $game->getName(),
+            'gameId' => $game->getId(),
+            'fields' => $fieldService->getFields($game),
+            'results' => $game->getResults()->toArray()
+        ]);
+    }
+}
